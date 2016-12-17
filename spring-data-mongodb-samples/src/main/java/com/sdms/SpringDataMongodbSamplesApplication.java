63ea3e922b6cdc65fd96bb14b59cae5d3bcc6746
william <385305466@qq.com>
2016-12-17 16:37:58 +0800

    create spring-data-mongodb-samples

---
+++ spring-data-mongodb-samples/src/main/java/com/sdms/SpringDataMongodbSamplesApplication.java
@@ -0,0 +1,12 @@
+package com.sdms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataMongodbSamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataMongodbSamplesApplication.class, args);
+	}
+}
